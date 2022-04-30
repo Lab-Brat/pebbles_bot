f2addf455bc2a9bab737c1464fb3e89e2fbe3d3e
Lab-Brat <kulikov.stepan.v@gmail.com>
2022-04-30 19:50:19 +0300

    add ansible.cfg to ignore host key checking, add ansible ad hoc command to carry out users commands

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import subprocess
 import telebot
 from telebot import types
 
-with open('/home/labbrat/bot_api.txt', 'r') as f:
+with open('/home/boink/bot_api.txt', 'r') as f:
     bot_api = f.read()
 bot = telebot.TeleBot(bot_api.strip('\n'))
 
-name = ''
-surname = ''
-age = 0
 @bot.message_handler(content_types=['text'])
 def start(message):
     if message.text == '/uptime':
@@ -32,21 +29,27 @@ def get_pass(message):
 def get_uname(message):
     global uname
     uname = message.text
+    bot.send_message(message.from_user.id, 'enter command')
+    bot.register_next_step_handler(message, get_cmd)
+
+def get_cmd(message):
+    global cmd
+    cmd = message.text
 
     keyboard = types.InlineKeyboardMarkup()
     key_yes = types.InlineKeyboardButton(text='yes', callback_data='yes')
     keyboard.add(key_yes)
     key_no= types.InlineKeyboardButton(text='no', callback_data='no')
     keyboard.add(key_no)
-    question = f"logging in machine {ip} with user {uname}, correct?"
+    question = f"logging into machine {ip} with user {uname}, running command {cmd}"
     bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == "yes":
-        ansible_cmd = ['ansible', 'all', '-i', ip, '--extra-vars',
-                      f'ansible_user={uname} ansible_password={paswd}', '-a' 'uptime']
+        ansible_cmd = ['ansible', 'all', '-i', f'{ip},', '--extra-vars',
+                      f'ansible_user={uname} ansible_password={paswd}', '-a', cmd]
         ram = subprocess.check_output(ansible_cmd).decode('ascii')
         bot.send_message(call.message.chat.id, ram)
     elif call.data == "no":
