0cb3ae82e177e0cfca60a6c1579c5cc0a5c071db
Lab-Brat <xoovoox@protonmail.com>
2022-05-03 23:34:14 +0300

    remove ansible for now, use paramiko instead

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import subprocess
 import telebot
 from telebot import types
 from datetime import datetime as dt
+from tools import Tools
 
 linux_user = subprocess.getoutput("echo $USER")
+tt = Tools()
 
 with open(f"/home/{linux_user}/bot_api.txt", 'r') as f:
     bot_api = f.read()
 bot = telebot.TeleBot(bot_api.strip('\n'))
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -19,68 +22,75 @@ def help(message):
     l0 = "Commands that Pebbles know:\n"
     c1 = "/start -----------> display start message\n"
     c2 = "/help ------------> display help message\n"
-    c3 = "/send_cmd ---> send a command to a Linux host"
-    help_message = f"{l0}{c1}{c2}{c3}"
+    c3 = "/login ---> send a command to a Linux host\n"
+    c4 = "/logout ---> terminate ssh session\n"
+    c5 = "/run ----------------> run a linux command"
+    help_message = f"{l0}{c1}{c2}{c3}{c4}{c5}"
     bot.reply_to(message, help_message)
     print(f'[{message.from_user.id} called /help method at {dt.now()}]')
 
+@bot.message_handler(commands=['logout'])
+def logout(message):
+    tt.ssh_disconnect()
+    bot.send_message(message.from_user.id, 'session terminated')
+
 @bot.message_handler(content_types=['text'])
 def start(message):
-    if message.text == '/send_cmd':
+    if message.text == '/login':
         bot.send_message(message.from_user.id, 'enter IP address <IP>:<port>')
         bot.register_next_step_handler(message, get_ip)
         print(f'[{message.from_user.id} called get_ip method at {dt.now()}]')
+    elif message.text == '/run':
+        bot.reply_to(message, "Please enter something: ")
+        bot.register_next_step_handler(message, run)
     else:
-        bot.send_message(message.from_user.id, 'I only know /send_cmd for now')
+        bot.send_message(message.from_user.id, 'I only know /login for now')
 
 def get_ip(message):
     global ip
-    ip = message.text
-    bot.send_message(message.from_user.id, 'enter password')
-    bot.register_next_step_handler(message, get_pass)
-    print(f'[{message.from_user.id} called get_pass method at {dt.now()}]')
-
-def get_pass(message):
-    global paswd
-    paswd = message.text
+    ip = message.text.split(':')
     bot.send_message(message.from_user.id, 'enter username')
     bot.register_next_step_handler(message, get_uname)
-    print(f'[{message.from_user.id} called get_uname method at {dt.now()}]')
+    print(f'[{message.from_user.id} called get_pass method at {dt.now()}]')
 
 def get_uname(message):
     global uname
     uname = message.text
-    bot.send_message(message.from_user.id, 'enter command')
-    bot.register_next_step_handler(message, get_cmd)
+    bot.send_message(message.from_user.id, 'enter password')
+    bot.register_next_step_handler(message, get_pass)
     print(f'[{message.from_user.id} called get_cmd method at {dt.now()}]')
 
-def get_cmd(message):
-    global cmd
-    cmd = message.text
+def get_pass(message):
+    global paswd
+    paswd = message.text
 
     keyboard = types.InlineKeyboardMarkup()
     key_yes = types.InlineKeyboardButton(text='yes', callback_data='yes')
     keyboard.add(key_yes)
     key_no= types.InlineKeyboardButton(text='no', callback_data='no')
     keyboard.add(key_no)
-    question1 = f"logging on to that machine {ip} with user `{uname}`, "
-    question2 = f"running command `{cmd}`"
-    question = question1 + question2
+    question = f"logging on to that machine {ip[0]}:{ip[1]} with user `{uname}`"
+
     bot.send_message(message.from_user.id, text=question, 
                      reply_markup=keyboard, parse_mode="markdown")
     print(f'[{message.from_user.id} was promted to verify data at {dt.now()}]')
+
+def run(message):
+    global cmd
+    cmd = message.text
+    cout = tt.ssh_cmd(cmd)
+    bot.send_message(message.from_user.id, cout)
+    print(f'[{message.from_user.id} called /run method at {dt.now()}]')
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == "yes":
-        ansible_cmd = ['ansible', 'all', '-i', f'{ip},', '--extra-vars',
-                      f'ansible_user={uname} ansible_password={paswd}', '-a', cmd]
-        ram = subprocess.check_output(ansible_cmd).decode('utf-8')
-        bot.send_message(call.message.chat.id, ram)
-        print(f'[user pressed YES on the keyboard and got ansible output at {dt.now()}]')
+        tt.ssh_connect(ip[0], ip[1], uname, paswd)
+        bot.send_message(call.message.chat.id, 'login success')
+        print(f'[user pressed YES on the keyboard at {dt.now()}]')
     elif call.data == "no":
-        bot.send_message(call.message.chat.id, 'call /send_cmd again please')
+        bot.send_message(call.message.chat.id, 'call /login again please')
         print(f'[user pressed NO on the keyboard] at {dt.now()}]')
 
 
---
+++ tools.py
@@ -0,0 +1,25 @@
+import paramiko
+
+class Tools():
+    def __init__(self):
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    def ssh_connect(self, host, port, uname, pwd):
+        self.client.connect(host, port=port, username=uname, password=pwd)
+
+    def ssh_disconnect(self):
+        self.client.close()
+
+    def ssh_cmd(self, cmd):
+        stdin, stdout, stderr = self.client.exec_command(cmd)
+        stdout_raw = [line.strip('\n') for line in stdout]
+        stdout_fin = ''
+        for part in stdout_raw:
+            stdout_fin += part + '\n'
+
+        return stdout_fin
+
+
+if __name__ == '__main__':
+    pass
