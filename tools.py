be79d3be2300d05e3a17c03c28b84d320a16b177
Lab-Brat <xoovoox@protonmail.com>
2022-05-07 20:11:29 +0300

    add comments

--- tools.py
+++ tools.py
@@ -1,11 +1,16 @@
+import fabric
 import paramiko
 
 class Tools():
     def __init__(self):
+        # instantiate paramiko ssh client
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     def ssh_connect(self, host, port, uname, pwd):
+        '''
+        Establish ssh connection
+        '''
         self.client.connect(host, port=port, username=uname, password=pwd)
         try:
             self.client.connect(host, port=port, username=uname, password=pwd)
@@ -18,19 +23,33 @@ class Tools():
             return 'time'
 
     def ssh_disconnect(self):
+        '''
+        Terminate ssh connection
+        '''
         self.client.close()
 
     def ssh_cmd(self, cmd):
+        '''
+        Run a linux command supplied by the user,
+        output standard output and standard error
+        '''
         stdin, stdout, stderr = self.client.exec_command(cmd)
         sout = self.translate_output(stdout)
         serr = self.translate_output(stderr)
         return sout, serr
 
-    def translate_output(self, output):
+    def translate_output(self, output, kind='all'):
+        '''
+        Make output human-readible
+        '''
         stdout_raw = [line.strip('\n') for line in output]
         stdout_fin = ''
-        for part in stdout_raw:
-            stdout_fin += part + '\n'
+        if kind == 'all':
+            for part in stdout_raw:
+                stdout_fin += part + '\n'
+        elif kind == 'last':
+            for part in stdout_raw:
+                stdout_fin = part
         return stdout_fin
 
 
