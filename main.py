8b03ad4fa461e5423ba001c708091626cc014204
Lab-Brat <xoovoox@protonmail.com>
2023-03-10 16:15:33 +0300

    Read info from pebbles.yaml
    Remove old way of dealing with api_key and whitelist, and replace
    it with a yaml configuration file.

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+import yaml
+from pathlib import Path
 from pebbles import Pebbles
 
+config = f"{str(Path.home())}/pebbles.yaml"
 
-def reader(file):
-    try:
-        with open(file) as f:
-            return f.read().split("\n")
-    except FileNotFoundError:
-        print(f"{file} was not found")
+
+def reader():
+    if Path(config).is_file():
+        try:
+            with open(config, "r") as config_file:
+                return yaml.safe_load(config_file)
+        except:
+            print(f"Error opening {config}")
+    else:
+        with open(config, "w") as config_file:
+            yaml.dump(
+                {"pebbles": {"api_key": "API_KEY", "whitelist": ["USER_ID"]}},
+                config_file,
+                default_flow_style=False,
+            )
+        print(
+            f"Configuration file template created at {config}\n"
+            f"please edit it and consult README for details"
+        )
 
 
 if __name__ == "__main__":
-    bot_api = reader("pebbles_api")[0]
-    whitelist = reader("pebbles_whitelist")
-
-    Pebbles(bot_api, whitelist)
+    read_yaml = reader()
+    Pebbles(read_yaml["pebbles"]["api_key"], read_yaml["pebbles"]["whitelist"])
